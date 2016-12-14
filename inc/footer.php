c20823654b323f06ea17cb87741a8c19fcd4b714
Ryley Ameden <ryley@ameden.me>
2016-12-14 15:20:02 -0500

    Update 2016-12-14T20:20:00.131Z

---
+++ inc/footer.php
@@ -0,0 +1,48 @@
+<div id="about-modal" class="uk-modal">
+    <div class="uk-modal-dialog uk-animation-slide-bottom">
+        <a class="uk-modal-close uk-close"></a>
+        <div class="uk-content uk-margin-top uk-margin-bottom">
+          <div class="modal-header uk-text-center">
+            <img class="uk-border-circle" src="assets/media/head.jpg" alt="" width="100">
+            <h2 class="uk-margin-medium-top">Hi, I'm Ryley.</h2>
+              <div class="socail-modal uk-flex uk-flex-center">
+                <div>
+                  <a class="social-github" href="https://github.com/rameden">
+                    <svg class="icon">
+                      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#github"></use>
+                    </svg>
+                  </a>
+                </div>
+                <div>
+                  <a class="social-twitter" href="https://twitter.com/rameden">
+                    <svg class="icon">
+                      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#twitter"></use>
+                    </svg>
+                  </a>
+                </div>
+                <div>
+                  <a class="social-facebook" href="https://facebook.com/ryley.ameden">
+                    <svg class="icon">
+                      <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#facebook"></use>
+                    </svg>
+                  </a>
+                </div>
+            </div>
+          </div>
+          <div class="modal-intro uk-margin-medium-top uk-grid">
+            <div class="uk-width-1-1 uk-margin-medium-bottom">
+              <p>I modernize, customize and other ‘ize-y things at <a href="http://dyn.com">Dyn</a>! My background involves pushing the limits of of what we can build on the backend and how we can experience it on the frontend. My passion is finding the best way to complete a project, optimization and teaching those around me.</p>
+            </div>
+            <div class="uk-width-small-1-1 uk-width-medium-1-2">
+              <h4>Working Together?</h4>
+              <p>My motto is: “Helping everyday business with development and marketing” Sound good? <a href="mailto:ryley@ameden.me">Contact me</a>.</p>
+            </div>
+            <div class="uk-width-small-1-1 uk-width-medium-1-2">
+              <h4>How'd you build this site?!</h4>
+              <p>Because I love open-source it's available for you to see. Find a bug? Report it!<a href="https://github.com/rameden/ameden.me"> View source</a>.</p>
+            </div>
+          </div>
+          <div class="uk-text-muted uk-text-center uk-margin-medium-top">Copyright © 2016 Ryley Ameden. MTFBWU</div>
+        </div>
+    </div>
+</div>
---
+++ inc/intro.php
@@ -0,0 +1,23 @@
+<div class="uk-vertical-align uk-height-viewport ">
+    <div class="intro-wrapper uk-vertical-align-middle uk-animation-slide-bottom">
+      <div class="intro-content">
+        <div class="intro-logo"><!--<img class="icon-logo" src="css/images/logo.svg" alt="Ameden">-->
+          <svg fill="url(#pattern)" class="icon-logo">
+              <use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#logo"></use>
+          </svg>
+        </div>
+        <div class="intro-copy">
+          <h1>Ryley Ameden</h1>
+          <div class="intro-sub">
+            <span class="uk-h2 uk-text-muted uk-margin-right">UI/UX Developer at</span>
+            <img class="dyn-logo" src="assets/media/dyn.svg" alt="Dyn">
+          </div>
+        </div>
+      </div>
+      <div class="nav uk-text-center uk-margin-medium-top">
+        <a href="/resume/" class="button button-resume">
+          <span>Resume</span>
+        </a>
+      </div>
+    </div>
+</div>
